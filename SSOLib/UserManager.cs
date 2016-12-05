1a66006a67b92f01c019c0a077026c62c2bf9210
Lindsay Morsillo <lindsay.morsillo@ircda.org>
2016-12-05 15:30:59 -0500

    Password Hashing

--- SSOLib/UserManager.cs
+++ SSOLib/UserManager.cs
@@ -5,17 +5,29 @@ using System.Text;
 using System.Threading.Tasks;
 using Scamps;
 using System.Configuration;
+using System.Security.Cryptography;
 
 namespace ircda.hobbes
 {
-    class UserManager
+    /// <summary>
+    /// This is a somewhat horrific object that isolates interaction with the user database
+    /// Not a high level of abstration, collects methods that create, update, retreive, delete users
+    /// </summary>
+    public class UserManager
     {
+        public static string UsernameCol = "username"; //!!! Use cleverer way to make easily configuarble...
+        public static string HashCol = "hash";
+        public static string SaltCol = "salt";
+        public static string UsersTableName = "users";
+
         protected string provider;
         protected string connectionString;
 
         //$$$ extern the SQL
         string findUserQuery = "select username from users where username = @nametofind;";
+        string getUserRecordQuery = "select * from users where username = @nametofind";
         string findUserQueryTok = "select username from users where username = '{0}';";
+
         public UserManager()
         {
             try
@@ -43,20 +55,140 @@ namespace ircda.hobbes
             dt.Provider = provider;
             dt.ConnectionString = connectionString;
             dt.OpenConnection();
-            string[] parameter = {"username", username};
+            string[] parameter = {UsernameCol, username};
             //!!! What is wrong with parameterized query?
             //dt.GetResultSet(findUserQuery, parameter);
-            //string.Format(findUserQueryTok, parameter);
             dt.GetResultSet(string.Format(findUserQueryTok, username));
 
             while (!dt.EOF)
             {
                 var row = dt.GetRow();
-                var nm = row["username"];
-                retVal.Add("username", nm.ToString());
+                var nm = row[UsernameCol];
+                retVal.Add(UsernameCol, nm.ToString());
                 dt.NextRow();
             }
             return retVal;
+        }
+        /// <summary>
+        /// Given username & other information authenticate the user from local db
+        /// </summary>
+        /// <param name="username">domain\user name or just name</param>
+        /// <param name="password"></param>
+        /// <param name="authmode"></param>
+        /// <returns> 0 - authenticated, 1 - user doesnt exist, 2 - user info populated but no data, 
+        /// 3 - invalid password, 4 - user is set to inactive/no_access</returns>
+        public int AuthenticateUser(string username, string password, int authmode)
+        {
+            int retval = -1; //Unintialized - undefined user
+            authmode = -1;
+            //Get the data first
+            //$$$ Centalize this - without locking up a connection 
+            DataTools dt = new DataTools();
+            dt.Provider = provider;
+            dt.ConnectionString = connectionString;
+            dt.OpenConnection();
+
+            string[] parameter = { "@username", username };
+            //!!! What is wrong with parameterized query?
+            dt.GetResultSet(getUserRecordQuery, parameter);
+
+            //Given we have data, verify secret
+
+            // return appropriate information - including authmode
+            //  -1 - uninitialized or undefined authentication profile
+            //   0 - active directory user
+            //   1 - local database user
+
+            return retval;
+        }
+        /// <summary>
+        /// Create a new user - base level simple insertion
+        /// uses salted hash for password authentication
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public int CreateNewUser(string username, string password, Dictionary<string,string>parameters = null)
+        {
+            int retval = -1;
+            DataTools dt = new DataTools();
+            dt.Provider = provider;
+            dt.ConnectionString = connectionString;
+            dt.OpenConnection();
+            Dictionary<string, string> items = null;
+            if (parameters != null)
+            {
+                items = new Dictionary<string, string>(parameters);
+            }
+            else
+            {
+                items = new Dictionary<string, string>();
+            }
+
+            items.Add(UsernameCol, username);
+            byte[] salt = GenerateSalt();
+            string saltString = Convert.ToBase64String(salt);
+            string hash = HashPassword(password, salt);
+            items.Add(HashCol, hash);
+            items.Add(SaltCol, saltString);
+
+            string insertStatement = dt.InsertStatement(items,UsersTableName); ;
+
+            return retval;
+        }
+        ///$$$ This might better be in auth module
+        /// <summary>
+        /// return salted hash of password
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public string HashPassword(string password, byte[] salt)
+        {
+            string retval = "";
+            
+            var hashedPassword = HashPasswordWithSalt(Encoding.UTF8.GetBytes(password), salt);
+            retval = Convert.ToBase64String(hashedPassword);
+            return retval;
+        }
+        /// <summary>
+        /// Create a 32 byte random salt
+        /// </summary>
+        /// <returns>byte array</returns>
+        public byte[] GenerateSalt()
+        {
+            const int saltLength = 32;
+            using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[saltLength];
+                randomNumberGenerator.GetBytes(randomNumber);
+                return randomNumber;
+            }
+        }
+        /// <summary>
+        /// Utility function - append byte array to another
+        /// </summary>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <returns>new array first followed by second</returns>
+        private byte[] Combine(byte[] first, byte[] second)
+        {
+            var ret = new byte[first.Length + second.Length];
+
+            Buffer.BlockCopy(first, 0, ret, 0, first.Length);
+            Buffer.BlockCopy(second, 0, ret, first.Length, second.Length);
+
+            return ret;
+        }
+
+        protected byte[] HashPasswordWithSalt(byte[] toBeHashed, byte[] salt)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var combinedHash = Combine(toBeHashed, salt);
+
+                return sha256.ComputeHash(combinedHash);
+            }
         }
         /*
         public static int authenticateUser(string username, string password, out int authmode)
---
+++ UnitTests/UserManagerTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ircda.hobbes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ircda.hobbes.Tests
+{
+    [TestClass()]
+    public class UserManagerTests
+    {
+        UserManager uut = null;
+        [TestMethod()]
+        public void GetUserTest()
+        {
+            uut = new UserManager();
+            Dictionary<string, string> usersfound = uut.GetUser("TestUser");
+        }
+
+        [TestMethod()]
+        public void AuthenticateUserTest()
+        {
+            uut = new UserManager();
+
+        }
+
+        [TestMethod()]
+        public void HashPasswordTest()
+        {
+            uut = new UserManager();
+            byte[] salt = uut.GenerateSalt();
+            string actualVal = uut.HashPassword("Welcome", salt);
+            string saltString = Convert.ToBase64String(salt);
+            Assert.IsNotNull(saltString);
+            Assert.IsTrue(salt.SequenceEqual(Convert.FromBase64String(saltString)));
+        }
+    }
+}
